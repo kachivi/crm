6546cf6533c87662e1f3155929d258bf8bb70026
Veeriah Ilavaluthi, Kachina Vazhuthi <kveeriahilavaluthi@dstworldwideservices.com>
2020-05-05 21:22:44 +0530

    Working version

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent,
+    NavbarComponent
+  ]
+})
+export class SharedModule { }
